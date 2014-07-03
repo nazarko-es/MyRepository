8c650fdf79ca81d97e58297833dca752d73b8dc9
Nikita Chebykin <nikita@chebyk.in>
2014-07-04 01:06:59 +0400

    add fixes

--- playlist.js
+++ playlist.js
@@ -1,8 +1,10 @@
 window.onload = init;
+
 function init() {
     var button = document.getElementById("addButton");
-    button.onclick = handleButtonClick();
+    button.onclick = handleButtonClick;
 }
+
 function handleButtonClick() {
     alert("Button was clicked!");
 }
